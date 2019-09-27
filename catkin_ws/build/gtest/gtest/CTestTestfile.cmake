709c522564373a45aebfa1126577b791d70db9ac
Sergey Kharlamov <serkhar@gmail.com>
2019-09-27 18:36:49 +0300

    rebase

--- catkin_ws/build/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/ch3rrydrunk/ros_hack/catkin_ws/build/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
